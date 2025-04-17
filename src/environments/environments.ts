e9c1cffe0e6406c3c1f7ba0f199a077612d68adf
kennethmuchiri7168 <kennethmuchiri7168@gmail.com>
2025-04-17 13:46:40 +0000

    firebase app

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyCaw7jzF6M5D5_aQsCld1TvW-4wHCdEUBQ',
+      authDomain: 'sample-firebase-app-7cda1.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-app-7cda1',
+      storageBucket: 'sample-firebase-app-7cda1.firebasestorage.app',
+      messagingSenderId: '164162796537',
+      appId: '1:164162796537:web:cbc1d10291610c32dadb28',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyCio-eM89Mhs-CD7AFjBLy9nPKVte1x1Gc',
+  debug_token: ''
+};
